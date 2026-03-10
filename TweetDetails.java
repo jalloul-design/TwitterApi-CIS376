518e1b658912569620513106674a9f4855f9868f
Mira Jalloul <jalloul@umich.edu>
2026-03-10 19:34:51 -0400

    Implement TweetDetails class with getters and setters
    Added fields and methods for tweet details.

--- TweetDetails.java
+++ TweetDetails.java
@@ -1,1 +1,58 @@
+package com.mira.twitterapi;
+
+public class TweetDetails {
+	
+	private String text; 
+	private String created_at; 
+	private String screen_name;
+	private Long id;
+	
+	public Long getId()
+	{
+		return id;
+	}
+	
+	public void setId(Long id)
+	{
+		this.id = id;
+	}
+	public String getScreen_name()
+	{
+	    return screen_name; 
+	}
+	
+	public void setScreen_name(String screen_name)
+	{
+		this.screen_name = screen_name;
+	}
+	
+	public String getCreated_at()
+	{
+		return created_at;
+	}
+	
+	public void setCreated_at(String created_at)
+	{
+		this.created_at = created_at;
+	}
+	
+	public String getText()
+	{
+		return text;
+	}
+	
+	public void setText(String text)
+	{
+		this.text = text;
+	}
+	
+	
+	public TweetDetails(Long id,String screen_name,String created_at,String text )
+	{
+	this.id= id;
+	this.screen_name = screen_name;
+	this.created_at = created_at;
+	this.text = text;	
+	}
+}
 
