854ea75db0bc8fb38de6fc52aaeda67fae913881
Mira Jalloul <jalloul@umich.edu>
2026-03-10 19:35:19 -0400

    Implement Tweets class with properties and methods
    Added fields and methods for tweet properties.

--- tweets.java
+++ tweets.java
@@ -1,1 +1,62 @@
+package com.mira.twitterapi;
 
+public class Tweets {
+	private String text; 
+	private String created_at; 
+	private Long id;
+	private TwitterUsersProfile user;
+	
+	 public Tweets() 
+	 {
+	 }
+	
+	 public String getCreated_at() 
+	 {
+		    return created_at;
+	 }
+
+	public void setCreated_at(String created_at) 
+	{
+		    this.created_at = created_at;
+	}
+	public Long getId() {
+	    return id;
+	}
+
+	public void setId(Long id) 
+	{
+	    this.id = id;
+	}
+
+	
+	public String getText() 
+	{
+	    return text;
+	}
+
+	public void setText(String text) 
+	{
+	    this.text = text;
+	}
+	// My get user by screen name wasn't working in my service class so springboot suggested to add it here in the Tweets class. 
+
+	public TwitterUsersProfile getUser() 
+	{
+    return user;
+	}
+
+	public void setUser(TwitterUsersProfile user)
+	{
+    this.user = user;
+	}
+
+	public Tweets(String text,String created_at,Long id)
+	{
+		this.created_at = created_at;
+		this.id = id;
+		this.text = text;
+	}
+
+
+	
+}
