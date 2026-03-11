d0cfae21e8915fce541193f0688948b296e58d79
Mira Jalloul <jalloul@umich.edu>
2026-03-10 21:01:00 -0400

    Add comments for clarity in TwitterLinks class
    Added comments to clarify the purpose of class fields and methods.

--- TwitterLinks.java
+++ TwitterLinks.java
@@ -2,11 +2,12 @@ package com.mira.twitterapi;
 
 import java.util.List;
 
-
+// Class that represents the external links from a tweet
 public class TwitterLinks {
-	private Long id;
-	private List<String> links;
+	private Long id; // ID number that is linked to the tweet
+	private List<String> links; // Creating a list of all external links that been extracted from the tweet text
 	
+	// Getter and Setter for Tweet ID
 	public Long getId()
 	{
 		return id;
@@ -16,7 +17,7 @@ public class TwitterLinks {
 	{
 		this.id = id;
 	}
-	
+	// Getter and Setter for list of extracted links
 	public List<String> getLinks()
 	{
 		return links;
@@ -26,7 +27,7 @@ public class TwitterLinks {
 	{
 		this.links =links;
 	}
-	
+	// Constructor used when creating a Twitter Link Object
 	public TwitterLinks (Long id,List<String> links)
 	{
 		this.id = id;
