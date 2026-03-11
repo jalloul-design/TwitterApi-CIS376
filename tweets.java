bce0d6df3a6f32678e1b368bd7f975c72a89161f
Mira Jalloul <jalloul@umich.edu>
2026-03-10 20:57:06 -0400

    Add comments for clarity in Tweets class
    Added comments to clarify the purpose of fields and methods in the Tweets class.

--- tweets.java
+++ tweets.java
@@ -1,15 +1,16 @@
 package com.mira.twitterapi;
 
 public class Tweets {
-	private String text; 
-	private String created_at; 
-	private Long id;
-	private TwitterUsersProfile user;
+	private String text; // Text of the tweet
+	private String created_at; // Time of when the tweet was created
+	private Long id; // Id number of the tweet
+	private TwitterUsersProfile user; // User name that is linked to the twitter account 
 	
+	// Default Constructor 
 	 public Tweets() 
 	 {
 	 }
-	
+	// Getter and setter for Created at
 	 public String getCreated_at() 
 	 {
 		    return created_at;
@@ -19,6 +20,8 @@ public class Tweets {
 	{
 		    this.created_at = created_at;
 	}
+	
+	// Getter and setter for Tweet ID
 	public Long getId() {
 	    return id;
 	}
@@ -28,7 +31,7 @@ public class Tweets {
 	    this.id = id;
 	}
 
-	
+	// Getter and Setter for text of a tweet
 	public String getText() 
 	{
 	    return text;
@@ -38,18 +41,20 @@ public class Tweets {
 	{
 	    this.text = text;
 	}
-	// My get user by screen name wasn't working in my service class so springboot suggested to add it here in the Tweets class. 
-
+	
+	
+	
+   // Getter function that will allow us to have access to the user information 
 	public TwitterUsersProfile getUser() 
 	{
     return user;
 	}
-
+	// My get user by screen name wasn't working in my service class so springboot suggested to add it here in the Tweets class.
 	public void setUser(TwitterUsersProfile user)
 	{
     this.user = user;
 	}
-
+    // Constructor used for creating tweet objects
 	public Tweets(String text,String created_at,Long id)
 	{
 		this.created_at = created_at;
