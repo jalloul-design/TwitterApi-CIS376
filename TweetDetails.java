ac9f6e4a4b30de74ebec12bace9450559619f34b
Mira Jalloul <jalloul@umich.edu>
2026-03-10 20:51:03 -0400

    Add comments to TweetDetails class members and methods
    Added comments to fields and methods for better clarity.

--- TweetDetails.java
+++ TweetDetails.java
@@ -1,12 +1,14 @@
 package com.mira.twitterapi;
 
+// This class represents the Tweet Details of a tweet made by a user
 public class TweetDetails {
 	
-	private String text; 
-	private String created_at; 
-	private String screen_name;
-	private Long id;
+	private String text; // Text of the tweet
+	private String created_at; // Time of when the tweet was created
+	private String screen_name; // Username of the person who posted the tweet
+	private Long id; // Id number of the tweet
 	
+	//Getter and Setter Function For ID
 	public Long getId()
 	{
 		return id;
@@ -16,6 +18,7 @@ public class TweetDetails {
 	{
 		this.id = id;
 	}
+	//Getter and Setter Function For User Screen Name
 	public String getScreen_name()
 	{
 	    return screen_name; 
@@ -25,7 +28,7 @@ public class TweetDetails {
 	{
 		this.screen_name = screen_name;
 	}
-	
+	//Getter and Setter Function For Created at
 	public String getCreated_at()
 	{
 		return created_at;
@@ -36,6 +39,7 @@ public class TweetDetails {
 		this.created_at = created_at;
 	}
 	
+	//Getter and Setter Function For Tweet Text
 	public String getText()
 	{
 		return text;
@@ -46,7 +50,7 @@ public class TweetDetails {
 		this.text = text;
 	}
 	
-	
+	//Creating a constructor for Tweetdetails
 	public TweetDetails(Long id,String screen_name,String created_at,String text )
 	{
 	this.id= id;
@@ -55,4 +59,3 @@ public class TweetDetails {
 	this.text = text;	
 	}
 }
-
